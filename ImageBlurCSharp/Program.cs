8b520b36990925812fcf562071b1b2637d5dafd5
Eric Crescioni <ecam900@live.com>
2017-02-17 16:32:46 -0400

    Added comments and references.

--- ImageBlurCSharp/Program.cs
+++ ImageBlurCSharp/Program.cs
@@ -1,23 +1,13 @@
 ﻿using System;
 
+
+/* For every topic that I had to research, I wrote a REF down below (at the end of the
+ * document). They are all numbered and have a link to the resource. */
+
 namespace ImageBlurCSharp
 {
     class Program
     {
-
-        static public void OutputArray(int[,] multiarray)
-        {
-            for (int row = 0; row < multiarray.GetLength(0); row++)
-            {
-                for (int col = 0; col < multiarray.GetLength(1); col++)
-                {
-                    int s = multiarray[row, col];
-                    Console.Write(s);
-                }
-                Console.WriteLine("");
-            }
-        }
-
         static void Main(string[] args)
         {
             int[,] multiarray =     { { 0, 0, 0, 0 },
@@ -28,5 +18,28 @@ namespace ImageBlurCSharp
             OutputArray(multiarray);
             Console.ReadLine();
         }
+
+
+        // Method outputs 2d array to the console.
+        static void OutputArray(int[,] multiarray)
+        {
+            // REF#1 - Difference between .GetLength() and .Length()
+            for (int row = 0; row < multiarray.GetLength(0); row++)
+            {
+                for (int col = 0; col < multiarray.GetLength(1); col++)
+                {
+                    int s = multiarray[row, col];
+                    Console.Write(s);
+                }
+                Console.WriteLine("");
+            }
+        }
     }
 }
+
+
+/* REF:
+    - Difference between Array.GetLength() and Array.Length():
+    http://stackoverflow.com/questions/2044591/what-is-the-difference-between-array-getlength-and-array-length
+    
+*/
