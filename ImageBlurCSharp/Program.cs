f976e00b7602885a12490b976dfdca6bba10501a
Eric Crescioni <ecam900@live.com>
2017-02-17 13:31:06 -0400

    Add program.

--- ImageBlurCSharp/Program.cs
+++ ImageBlurCSharp/Program.cs
@@ -6,7 +6,23 @@ namespace ImageBlurCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] multiarray = { { 0, 0, 0, 0 },
+                                      { 1, 0, 0, 0 },
+                                      { 2, 0, 0, 0 },
+                                      { 3, 0, 0, 0 } };
+
+            for (int row = 0; row < multiarray.GetLength(0); row++)
+            {
+                for (int col = 0; col < multiarray.GetLength(1); col++)
+                {
+                    int s = multiarray[row, col];
+                    Console.Write(s);
+                }
+                Console.WriteLine("");
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
