9e6f2281692cd0fd2353e031493be1fc4a6a0308
Eric Crescioni <ecam900@live.com>
2017-02-17 21:58:39 -0400

    Add blur method. In progress.

--- ImageBlurCSharp/Program.cs
+++ ImageBlurCSharp/Program.cs
@@ -11,14 +11,42 @@ namespace ImageBlurCSharp
         static void Main(string[] args)
         {
             int[,] multiarray =     { { 0, 0, 0, 0 },
-                                      { 1, 0, 0, 0 },
-                                      { 2, 0, 0, 0 },
-                                      { 3, 0, 0, 0 } };
+                                      { 2, 0, 1, 0 },
+                                      { 3, 0, 0, 0 },
+                                      { 4, 0, 0, 0 } };
+
+
+            Blur(multiarray);
+
 
             OutputArray(multiarray);
             Console.ReadLine();
         }
 
+        public static void Blur(int[,] multiarray)
+        {
+            int[,] blurredArray = multiarray;
+
+            for (int row = 0; row < multiarray.GetLength(0); row++)
+            {
+                for (int col = 0; col < multiarray.GetLength(1); col++)
+                {
+                    int point = multiarray[row, col];
+
+
+                    // PROBLEM HERE! : OUT OF BOUNDS EXCEPTION. WORKING ON IT!
+                    if (point == 1)
+                    {
+                        blurredArray[row - 1, col] = 1;
+                        blurredArray[row + 1, col] = 1;
+                        blurredArray[row, col - 1] = 1;
+                        blurredArray[row, col + 1] = 1;
+                    };
+
+                }
+                OutputArray(blurredArray);
+            }
+        }
 
         // Method outputs 2d array to the console.
         static void OutputArray(int[,] multiarray)
@@ -34,6 +62,8 @@ namespace ImageBlurCSharp
                 Console.WriteLine("");
             }
         }
+
+
     }
 }
 
