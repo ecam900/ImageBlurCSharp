7b57cc86ce7259adb736aa678204835645812bef
Eric Crescioni <ecam900@live.com>
2017-02-20 20:37:58 -0400

    Add functional blur method. Takes edges into account.

--- ImageBlurCSharp/Program.cs
+++ ImageBlurCSharp/Program.cs
@@ -11,41 +11,48 @@ namespace ImageBlurCSharp
         static void Main(string[] args)
         {
             int[,] multiarray =     { { 0, 0, 0, 0 },
-                                      { 2, 0, 1, 0 },
-                                      { 3, 0, 0, 0 },
-                                      { 4, 0, 0, 0 } };
+                                      { 0, 0, 1, 0 },
+                                      { 0, 0, 0, 0 },
+                                      { 0, 1, 0, 0 } };
 
+           
+            Console.WriteLine("Old array: ");
+            Console.WriteLine(" ");
+            OutputArray(multiarray);
 
+            Console.WriteLine(" ");
+            Console.WriteLine("New array: ");
+            Console.WriteLine(" ");
             Blur(multiarray);
 
-
-            OutputArray(multiarray);
             Console.ReadLine();
         }
 
         public static void Blur(int[,] multiarray)
         {
-            int[,] blurredArray = multiarray;
+            int[,] blurredArray = (int[,])multiarray.Clone();
 
             for (int row = 0; row < multiarray.GetLength(0); row++)
             {
                 for (int col = 0; col < multiarray.GetLength(1); col++)
                 {
+
                     int point = multiarray[row, col];
 
-
-                    // PROBLEM HERE! : OUT OF BOUNDS EXCEPTION. WORKING ON IT!
                     if (point == 1)
                     {
-                        blurredArray[row - 1, col] = 1;
-                        blurredArray[row + 1, col] = 1;
-                        blurredArray[row, col - 1] = 1;
-                        blurredArray[row, col + 1] = 1;
-                    };
-
+                        if (row - 1 >= 0)
+                            blurredArray[row - 1, col] = 1;
+                        if ( row + 1 < multiarray.GetLength(0) )
+                            blurredArray[row + 1, col] = 1;
+                        if (col - 1 >= 0)
+                            blurredArray[row, col - 1] = 1;
+                        if (col + 1 < multiarray.GetLength(1))
+                            blurredArray[row, col + 1] = 1;
+                    }
                 }
-                OutputArray(blurredArray);
             }
+            OutputArray(blurredArray);
         }
 
         // Method outputs 2d array to the console.
